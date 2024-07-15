6d70d53908709297b6815fa3c02c7d80e544b856
abeciana1 <alex.beciana@gmail.com>
2024-07-15 12:54:28 -0600

    Removed boilerplate starter project configuration and components

--- next.config.js
+++ next.config.js
@@ -1,26 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    const sourcesRequiringAuthToken = [
-      "/", "/:slug*",
-    ];
-
-    if (process.env.NEXT_PUBLIC_BUTTER_CMS_API_KEY) {
-      return [
-        {
-          source: "/missing-token",
-          destination: "/",
-          permanent: false,
-        }
-      ];
-    } else {
-      return sourcesRequiringAuthToken.map(source => ({
-        source: source,
-        destination: "/missing-token",
-        permanent: false,
-      }));
-    }
-  },
   images: {
     domains: ["cdn.buttercms.com"],
     dangerouslyAllowSVG: true,
