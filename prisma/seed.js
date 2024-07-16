456c994cfd113fa917fcff3278a5cb135843bbbe
abeciana1 <alex.beciana@gmail.com>
2024-07-16 11:54:54 -0600

    Created seed file

---
+++ prisma/seed.js
@@ -0,0 +1,17 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const main = async () => {
+    
+}
+
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
