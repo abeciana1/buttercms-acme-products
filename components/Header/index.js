adafec66098101cabcbbdc1465983640e7342505
abeciana1 <alex.beciana@gmail.com>
2024-07-17 16:47:46 -0600

    Created main menu component

---
+++ components/Header/index.js
@@ -0,0 +1,127 @@
+import { useState } from 'react'
+import useResponsiveness from '@/lib/hooks/useResponsiveness'
+import Hamburger from 'hamburger-react'
+import cx from 'classnames'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FaCartShopping, FaCircleUser } from "react-icons/fa6";
+
+const Header = ({ mainMenu }) => {
+    const [expanded, setExpanded] = useState(false)
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    const { isMobile, isTablet, isDesktop } = useResponsiveness() || {}
+
+    console.log(mainMenu);
+    
+    const toggleExpand = () => {
+        if (isDesktop) {
+            setExpanded(true)
+        }
+    }
+
+    const toggleCollapse = () => {
+        if (isDesktop) {
+            setExpanded(false)
+        }
+    }
+
+    return (
+        <header className={cx('flex justify-between pt-5 px-5',{
+            ['flex-row items-center']: isDesktop,
+            ['flex-col items-start']: isMobile,
+            ['flex-col items-center']: isTablet
+        })}>
+            <Link
+                href='/'
+                title='Homepage'
+            >
+                <Image
+                    src={mainMenu.logo}
+                    width={200}
+                    height={106}
+                    alt="ACME logo"
+                />
+            </Link>
+            <nav
+                className={cx({
+                    ['bg-yellow-50 h-screen w-full']: ((isMobile || isTablet) && mobileMenuOpen)
+                })}
+            >
+                {(mobileMenuOpen || isDesktop) &&
+                    <ul
+                        className={cx('flex', {
+                            ['items-center justify-between gap-10']: isDesktop,
+                            ['flex-col pt-5 gap-5']: isMobile || isTablet
+                        })}
+                    >
+                        {mainMenu?.menu_items_first?.map((menuItem) => {
+                            return (
+                                <li key={menuItem.meta.id} className={cx('w-full', {
+                                    ['text-xl']: isDesktop,
+                                    ['text-2xl']: isMobile || isTablet
+                                })}>
+                                    <Link
+                                        href={'/' + menuItem.url}
+                                    >
+                                        {menuItem.label}
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                        {mainMenu?.menu_items_dd &&
+                            <li
+                                className={cx('relative', {
+                                    ['text-xl']: isDesktop,
+                                    ['text-2xl']: isMobile || isTablet
+                                })}
+                                onMouseEnter={toggleExpand}
+                                onMouseLeave={toggleCollapse}
+                            >
+                                <div>Categories</div>
+                                {(expanded || isMobile || isTablet) &&
+                                    <ul
+                                        className={cx('', {
+                                            ['absolute bg-white shadow-md p-4 w-60 rounded-md mt-2']: expanded
+                                        })}
+                                    >
+                                        {mainMenu?.menu_items_dd?.map((menuItem) => {
+                                            return(
+                                                <li key={menuItem.meta.id} className='ml-5 pt-2'>
+                                                    <Link
+                                                        title={menuItem.category_name}
+                                                        href={'/' + menuItem.category_slug}
+                                                    >
+                                                        {menuItem.category_name}
+                                                    </Link>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                }
+                            </li>
+                        }
+                    </ul>
+                }
+                {(isMobile || isTablet) &&
+                    <div className='absolute top-5 right-5 z-50'>
+                        <Hamburger
+                            toggled={mobileMenuOpen}
+                            toggle={setMobileMenuOpen}
+                            color='#101820'
+                        />
+                    </div>
+                }
+            </nav>
+            <div className='flex flex-row gap-10'>
+                <Link href='/cart'>
+                    <FaCartShopping size={30} />
+                </Link>
+                <Link href='/my-account'>
+                    <FaCircleUser size={30} />
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Header
