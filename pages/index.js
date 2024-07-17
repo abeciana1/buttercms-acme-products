8e9e3e9d29f0dc96e5db371c59a8744c2ae41da4
abeciana1 <alex.beciana@gmail.com>
2024-07-17 17:16:01 -0600

    Added getStaticProps func to fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
-import Butter from 'buttercms';
+import { getPageData } from '@/lib/api'
 
 const Home = (props) => {
-    console.log('props', props)
+    // console.log('props', props)
     return (
         <></>
     )
 }
 
+export const getStaticProps = async () => {
+    let pageData = await getPageData('homepage')
+    console.log('page data', pageData)
+    return {
+        props: {}
+    }
+}
 
 
 export default Home
