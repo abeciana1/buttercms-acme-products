1e69308264841acfca497940f1d3dd7e16bad9a2
abeciana1 <alex.beciana@gmail.com>
2024-07-18 12:42:52 -0600

    Created HeroSection component

--- components/_page-sections/HeroSection/index.js
+++ components/_page-sections/HeroSection/index.js
@@ -1,8 +1,60 @@
+import { PageLayoutWrapper } from '@/components/_layouts'
+import Image from 'next/image'
+import { LinkLookLikeButton } from '@/components/_inputs/Links'
 
-const HeroSection = (props) => {
-    console.log(props)
+// {
+//     "headline": "ACME: Unleash the Fun!",
+//     "body_text": "Dive into a world of quirky and innovative products designed to make you smile. Shop ACME for your next big laugh and brilliant solution!",
+//     "button_color": {
+//         "meta": {
+//             "id": 763563
+//         },
+//         "brand_color": "Red",
+//         "brand_colors_dd": "red"
+//     },
+//     "cta_label": "Shop our categories!",
+//     "cta_href": "c/all",
+//     "image": "https://cdn.buttercms.com/UcRd3hxSSEWPed6VCnoC",
+//     "image_alt_text": "ACME - This might hurt seal",
+//     "image_width": 150,
+//     "image_height": 150
+// }
+
+const HeroSection = ({
+    headline,
+    bodyText,
+    image,
+    imageAltText,
+    imageWidth,
+    imageHeight,
+    ctaLabel,
+    ctaHref,
+    buttonColor
+}) => {
     return (
-        <>hello</>
+        <PageLayoutWrapper>
+            <section className='flex items-center gap-10 justify-center'>
+                <div className='w-1/2'>
+                    <h1>{headline}</h1>
+                    <div className='text-xl'>{bodyText}</div>
+                    <div className='pt-5'>
+                        <LinkLookLikeButton
+                            linkText={ctaLabel}
+                            linkHref={ctaHref}
+                            color={buttonColor.brand_color}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <Image
+                        src={image}
+                        alt={imageAltText}
+                        width={imageWidth}
+                        height={imageHeight}
+                    />
+                </div>
+            </section>
+        </PageLayoutWrapper>
     )
 }
 
