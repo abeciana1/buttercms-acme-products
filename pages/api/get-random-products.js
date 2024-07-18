ab8ebab4dd6207341df2e9453c66a196fb22be85
abeciana1 <alex.beciana@gmail.com>
2024-07-18 16:28:59 -0600

    Created api route to return random product data

---
+++ pages/api/get-random-products.js
@@ -0,0 +1,15 @@
+import prisma from '@/lib/prisma'
+
+const handler = async (req, res) => {
+    if (req.method === 'GET') {
+        const productsCount = await prisma.product.count();
+        const skip = Math.floor(Math.random() * productsCount);
+        const products = await prisma.product.findMany({
+            take: 4,
+            skip: skip
+        })
+        res.status(200).json(products)
+    }
+}
+
+export default handler
