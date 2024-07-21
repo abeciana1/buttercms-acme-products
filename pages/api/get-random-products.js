ac272a9d65d9a85e41101e6df86cc562a6fe7ca2
abeciana1 <alex.beciana@gmail.com>
2024-07-21 11:08:41 -0600

    Removed unused code

--- pages/api/get-random-products.js
+++ pages/api/get-random-products.js
@@ -2,11 +2,8 @@ import prisma from '@/lib/prisma'
 
 const handler = async (req, res) => {
     if (req.method === 'GET') {
-        const productsCount = await prisma.product.count();
-        // const skip = Math.floor(Math.random() * productsCount);
         const products = await prisma.product.findMany({
             take: 9,
-            // skip: skip
         })
         res.status(200).json(products)
     }
