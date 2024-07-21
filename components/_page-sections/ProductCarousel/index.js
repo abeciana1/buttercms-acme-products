8f6479d882715d5ec5ba7ea13a2f912dabe89a17
abeciana1 <alex.beciana@gmail.com>
2024-07-21 11:07:32 -0600

    Refined ProductCarousel component

--- components/_page-sections/ProductCarousel/index.js
+++ components/_page-sections/ProductCarousel/index.js
@@ -1,26 +1,37 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Preloader from '@/components/Preloader'
-import { Carousel } from 'react-responsive-carousel';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import ProductCard from '@/components/_products/ProductCard'
 
-const getSlidePercentage = () => {
-    if (window.innerWidth >= 1024) {
-      return 33.33; // 3 slides per page on larger screens
-    } else if (window.innerWidth >= 768) {
-      return 50; // 2 slides per page on medium screens
-    } else {
-      return 100; // 1 slide per page on small screens
-    }
-};
-
 const ProductCarousel = ({
-    carouselTitle,
-    children
+    carouselTitle
 }) => {
     const [ products, setProducts ] = useState([])
     const [ isLoading, setIsLoading ] = useState(false)
+    const [ slidesPerPage, setSlidesPerPage ] = useState(getSlidesPerPage())
+    // const [ currentPage, setCurrentPage ] = useState(0)
+    const [ startIdx , setStartIdx ] = useState(0)
+    const [ endIdx, setEndIdx ] = useState(slidesPerPage)
+
+    console.log({ startIdx, endIdx });
+
+    function getSlidesPerPage() {
+        if (typeof window !== 'undefined') {
+            if (window.innerWidth >= 1050) {
+                return 3; // 3 slides per page on larger screens
+            } else if (window.innerWidth >= 640) {
+                return 2; // 2 slides per page on medium screens
+            } else {
+                return 1; // 1 slide per page on small screens
+            }
+        }
+        return 1;
+    }
+
+    useEffect(() => {
+        setSlidesPerPage(getSlidesPerPage());
+    }, [])
 
     useEffect(() => {
         const fetchProductData = async () => {
@@ -36,56 +47,75 @@ const ProductCarousel = ({
         }
         fetchProductData()
     }, [])
-    
+
+    useEffect(() => {
+        const adjustSlidesPerPage = () => {
+            setSlidesPerPage(getSlidesPerPage());
+            setStartIdx(0);
+            setEndIdx(getSlidesPerPage());
+        }
+
+        if (window) {
+            window.addEventListener('resize', adjustSlidesPerPage)
+            window.addEventListener('orientationchange', adjustSlidesPerPage)
+        }
+        return () => {
+            window.removeEventListener('resize', adjustSlidesPerPage)
+            window.removeEventListener('orientationchange', adjustSlidesPerPage)
+        }
+    }, [])
+
+    function nextSlide() {
+        setStartIdx((prevStartIdx) => {
+            const newStartIdx = prevStartIdx + slidesPerPage;
+            return newStartIdx >= products.length ? 0 : newStartIdx;
+        });
+        setEndIdx((prevEndIdx) => {
+            const newEndIdx = prevEndIdx + slidesPerPage;
+            return newEndIdx > products.length ? slidesPerPage : newEndIdx;
+        });
+    }
+
+        function prevSlide() {
+        setStartIdx((prevStartIdx) => {
+            const newStartIdx = prevStartIdx - slidesPerPage;
+            return newStartIdx < 0 ? Math.max(0, products.length - slidesPerPage) : newStartIdx;
+        });
+
+        setEndIdx((prevEndIdx) => {
+            const newEndIdx = prevEndIdx - slidesPerPage;
+            return newEndIdx < slidesPerPage ? products.length : newEndIdx;
+        });
+    }
+
     return (
-        <section className="">
-            <div className='sr-only'>{carouselTitle}</div>
-            <div
-                className='font-optiscript text-4xl'
-            >{carouselTitle}</div>
-            <section className='relative'>
-                <Carousel
-                showArrows
-                renderArrowPrev={(onClickHandler, hasPrev, label) =>
-                    hasPrev && (
-                    <button type="button" onClick={onClickHandler} title={label} style={arrowStyles}>
-                        <FaArrowLeft color='#000' size={30} />
-                    </button>
-                    )
-                }
-                renderArrowNext={(onClickHandler, hasNext, label) =>
-                    hasNext && (
-                    <button type="button" onClick={onClickHandler} title={label} style={arrowStyles}>
-                        <FaArrowRight color='#000' size={30} />
-                    </button>
-                    )
-                }
-                showStatus={false}
-                showIndicators={false}
-                showThumbs={false}
-                centerMode
-                centerSlidePercentage={getSlidePercentage()}
-                >
-                    {products.length < 1 && <Preloader />}
-                    {products.length > 0 &&
-                        products.map((product, index) => <ProductCard key={product.sku + index} product={product} /> )
-                    }
-                </Carousel>
-            </section>
+    <section className='relative'>
+        <div className='sr-only'>{carouselTitle}</div>
+        <div
+            className='font-optiscript text-4xl'
+        >{carouselTitle}</div>
+        <div className='pt-5 flex gap-10'>
+            <button onClick={prevSlide} className="z-10 bg-gray-700 text-white p-2 rounded-full">
+                <FaArrowLeft size={30} />
+            </button>
+            <button onClick={nextSlide} className="z-10 bg-gray-700 text-white p-2 rounded-full">
+                <FaArrowRight size={30} />
+            </button>
+        </div>
+        <section className='pt-5'>
+            {isLoading && <Preloader />}
+            {(!isLoading && products.length > 0) &&
+                <section className='flex gap-5 justify-center'>
+                    {products.slice(startIdx, endIdx).map((product, index) => {
+                    return (
+                        <ProductCard key={product.sku + index} product={product} />
+                    )})}
+                </section>
+            }
         </section>
-    )
+    </section>
+    );
 }
 
-const arrowStyles = {
-    position: 'absolute',
-    zIndex: 2,
-    top: 'calc(50% - 15px)',
-    width: 30,
-    height: 30,
-    cursor: 'pointer',
-    backgroundColor: 'transparent',
-    border: 'none',
-    outline: 'none',
-};
 
 export default ProductCarousel
