8e6d4551ba5a067b27d2d93004230c4fa5c2f540
abeciana1 <alex.beciana@gmail.com>
2024-07-21 11:32:36 -0600

    Adjusted styling for mobile devices

--- components/_navigation/Header/index.js
+++ components/_navigation/Header/index.js
@@ -25,10 +25,10 @@ const Header = ({ mainMenu }) => {
     }
 
     return (
-        <header className={cx('flex justify-between pt-5 px-5',{
-            ['flex-row items-center']: isDesktop,
-            ['flex-col items-start']: isMobile,
-            ['flex-col items-center']: isTablet
+        <header className={cx('relative flex items-center justify-between py-5 px-5',{
+            ['flex-row']: isDesktop,
+            ['flex-col justify-center']: isMobile,
+            ['flex-col']: isTablet
         })}>
             <Link
                 href='/'
@@ -39,6 +39,9 @@ const Header = ({ mainMenu }) => {
                     width={200}
                     height={106}
                     alt="ACME logo"
+                    className={cx({
+                        ['h-10 w-auto']: isMobile
+                    })}
                 />
             </Link>
             <nav
@@ -47,65 +50,67 @@ const Header = ({ mainMenu }) => {
                 })}
             >
                 {(mobileMenuOpen || isDesktop) &&
-                    <ul
-                        className={cx('flex', {
-                            ['items-center justify-between gap-10']: isDesktop,
-                            ['flex-col pt-5 gap-5']: isMobile || isTablet
-                        })}
-                    >
-                        {mainMenu?.menu_items_first?.map((menuItem) => {
-                            return (
+                    <>
+                        <ul
+                            className={cx('flex', {
+                                ['items-center justify-between gap-10']: isDesktop,
+                                ['flex-col pt-5 gap-5']: isMobile || isTablet
+                            })}
+                        >
+                            {mainMenu?.menu_items_first?.map((menuItem) => {
+                                return (
+                                    <li
+                                        onMouseEnter={toggleCollapse}
+                                        key={menuItem.meta.id}
+                                        className={cx('w-full', {
+                                        ['text-xl']: isDesktop,
+                                        ['text-2xl']: isMobile || isTablet
+                                    })}>
+                                        <Link
+                                            href={'/' + menuItem.url}
+                                        >
+                                            {menuItem.label}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                            {mainMenu?.menu_items_dd &&
                                 <li
-                                    onMouseEnter={toggleCollapse}
-                                    key={menuItem.meta.id}
-                                    className={cx('w-full', {
-                                    ['text-xl']: isDesktop,
-                                    ['text-2xl']: isMobile || isTablet
-                                })}>
-                                    <Link
-                                        href={'/' + menuItem.url}
-                                    >
-                                        {menuItem.label}
-                                    </Link>
+                                    className={cx('relative', {
+                                        ['text-xl']: isDesktop,
+                                        ['text-2xl']: isMobile || isTablet
+                                    })}
+                                    onMouseEnter={toggleExpand}
+                                >
+                                    <div>Categories</div>
+                                    {(expanded || isMobile || isTablet) &&
+                                        <ul
+                                            onMouseLeave={toggleCollapse}
+                                            className={cx('', {
+                                                ['absolute bg-white shadow-md p-4 w-60 rounded-md mt-2']: expanded
+                                            })}
+                                        >
+                                            {mainMenu?.menu_items_dd?.map((menuItem) => {
+                                                return(
+                                                    <li key={menuItem.meta.id} className='ml-5 pt-2'>
+                                                        <Link
+                                                            title={menuItem.category_name}
+                                                            href={'/' + menuItem.category_slug}
+                                                        >
+                                                            {menuItem.category_name}
+                                                        </Link>
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    }
                                 </li>
-                            )
-                        })}
-                        {mainMenu?.menu_items_dd &&
-                            <li
-                                className={cx('relative', {
-                                    ['text-xl']: isDesktop,
-                                    ['text-2xl']: isMobile || isTablet
-                                })}
-                                onMouseEnter={toggleExpand}
-                            >
-                                <div>Categories</div>
-                                {(expanded || isMobile || isTablet) &&
-                                    <ul
-                                        onMouseLeave={toggleCollapse}
-                                        className={cx('', {
-                                            ['absolute bg-white shadow-md p-4 w-60 rounded-md mt-2']: expanded
-                                        })}
-                                    >
-                                        {mainMenu?.menu_items_dd?.map((menuItem) => {
-                                            return(
-                                                <li key={menuItem.meta.id} className='ml-5 pt-2'>
-                                                    <Link
-                                                        title={menuItem.category_name}
-                                                        href={'/' + menuItem.category_slug}
-                                                    >
-                                                        {menuItem.category_name}
-                                                    </Link>
-                                                </li>
-                                            )
-                                        })}
-                                    </ul>
-                                }
-                            </li>
-                        }
-                    </ul>
+                            }
+                        </ul>
+                    </>
                 }
                 {(isMobile || isTablet) &&
-                    <div className='absolute top-5 right-5 z-50'>
+                    <div className='absolute top-3 left-5 z-50'>
                         <Hamburger
                             toggled={mobileMenuOpen}
                             toggle={setMobileMenuOpen}
@@ -114,10 +119,12 @@ const Header = ({ mainMenu }) => {
                     </div>
                 }
             </nav>
-            <div className='flex flex-row gap-10 items-center'>
-                <Cart cartItems={5} cartTotal={35.46} />
+            <div className={cx('flex flex-row gap-10 items-center', {
+                ['absolute right-2 gap-5 top-6']: (isMobile || isTablet)
+            })}>
+                <Cart size={(isMobile || isTablet) ? 30 : 35} cartItems={5} cartTotal={35.46} />
                 <Link title='My Account' href='/my-account'>
-                    <FaCircleUser size={35} />
+                    <FaCircleUser size={(isMobile || isTablet) ? 30 : 35} />
                 </Link>
             </div>
         </header>
