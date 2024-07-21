fecad15143abd73eaecda110d69dc835e3adebd6
abeciana1 <alex.beciana@gmail.com>
2024-07-21 11:32:22 -0600

    Made size prop dynamic

--- components/_navigation/Cart/index.js
+++ components/_navigation/Cart/index.js
@@ -2,13 +2,14 @@ import Link from 'next/link'
 import { FaCartShopping } from "react-icons/fa6";
 
 const Cart = ({
+    size,
     cartItems = 0,
     cartTotal = 0
 }) => {
     return(
         <div className='relative'>
             <Link title='Cart page' href='/cart'>
-                <FaCartShopping size={35} />
+                <FaCartShopping size={size} />
             </Link>
             {cartItems > 0 &&
                 <div
