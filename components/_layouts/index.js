21798b9b267098f291bced3ac030164f9888f1a5
abeciana1 <alex.beciana@gmail.com>
2024-07-23 08:26:58 -0600

    Added vertical padding

--- components/_layouts/index.js
+++ components/_layouts/index.js
@@ -1,6 +1,6 @@
 export const PageLayoutWrapper = ({ children }) => {
     return (
-        <section className='px-5 md:px-10'>
+        <section className='px-5 md:px-10 py-5 md:py-10'>
             { children }
         </section>
     )
