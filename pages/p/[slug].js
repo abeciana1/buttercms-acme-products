eac5216f608bab120ca44963ff05bbd5b9b6681f
abeciana1 <alex.beciana@gmail.com>
2024-07-23 12:38:58 -0600

    Adjusted responsiveness && Added ATC form and page elements

--- pages/p/[slug].js
+++ pages/p/[slug].js
@@ -1,49 +1,58 @@
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { PageLayoutWrapper } from '@/components/_layouts'
 import { getPageData }  from '@/lib/api'
 import { NextSeo, ProductJsonLd } from 'next-seo'
+import Image from 'next/image'
+import Link from 'next/link'
 import axios from 'axios'
-
-// {
-//     "product_name": "Mini-Rocket",
-//     "product_sku": "AMR2200",
-//     "seo": {
-//         "title": "ACME | Mini-Rocket",
-//         "description": "Launch into fun with the ACME Mini-Rocket. Compact, powerful, and perfect for hobbies and pranks. ACME: Innovating excitement and joy.",
-//         "json_ld_schema": "product"
-//     },
-//     "product_image": [
-//         {
-//             "image": "https://cdn.buttercms.com/xnOsGBTxQYm74xb7PySW",
-//             "image_width": 1200,
-//             "image_height": 534
-//         }
-//     ],
-//     "category": {
-//         "meta": {
-//             "id": 763121
-//         },
-//         "category_name": "Transportation",
-//         "category_slug": "c?category=transportation"
-//     },
-//     "headline": "This is the future of Transportation",
-//     "description": "This single-seat transport (SST) will get you there in record time, wherever there happens to be. Hop on, find a sturdy leather belt to hold you in place, and \"assume the position\"! Shear years off your travel time and let everyone know you've arrived with the signature \"BOOM\" as you cross the sound barrier!",
-//     "additional_description": "",
-//     "warning": "Safety harness sold separately."
-// }
+import useResponsiveness from '@/lib/hooks/useResponsiveness'
+import Breadcrumbs from '@/components/_products/Breadcrumbs'
+import cx from 'classnames'
+import { GoAlertFill } from "react-icons/go";
+import ATCForm from '@/components/_forms/ATCForm'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import Accordion from '@/components/_page-sections/Accordion'
+import ProductCarousel from '@/components/_page-sections/ProductCarousel'
+import { slugify } from '@/lib/helper-functions'
 
 const CatchAll = ({
     seo,
     body,
     product
 }) => {
-    console.log('body', body?.body)
-    console.log('product', product);
+    const { isDesktop, isTablet, isMobile } = useResponsiveness() || {}
+    const [ startIdx , setStartIdx ] = useState(0)
+    const [ endIdx, setEndIdx ] = useState(1)
+    const imageLength = body?.body?.product_image?.length
+    const slugifiedCategoryName = slugify(body?.body?.category?.category_name)
+    console.log('testing', body?.body)
     const productImages = useMemo(() => {
         body?.product_images?.map((prodImage) => {
             return prodImage.image
         })
     }, [])
+
+    const nextSlide = () => {
+        setStartIdx((prevStartIdx) => {
+            const newStartIdx = prevStartIdx + 1;
+            return newStartIdx >= imageLength ? 0 : newStartIdx;
+        });
+        setEndIdx((prevEndIdx) => {
+            const newEndIdx = prevEndIdx + 1;
+            return newEndIdx > imageLength ? 1 : newEndIdx;
+        });
+    }
+
+    const prevSlide = () => {
+        setStartIdx((prevStartIdx) => {
+            const newStartIdx = prevStartIdx - 1;
+            return newStartIdx < 0 ? Math.max(0, imageLength - 1) : newStartIdx;
+        });
+        setEndIdx((prevEndIdx) => {
+            const newEndIdx = prevEndIdx - 1;
+            return newEndIdx < 1 ? imageLength : newEndIdx;
+        });
+    }
 
     return (
         <>
@@ -62,7 +71,7 @@ const CatchAll = ({
                 mpn={body?.product_sku}
                 offers={[
                     {
-                        price: (product?.discount > 0) ? product?.discountPrice : product?.price,
+                        price: `$${(product?.discount > 0) ? product?.discountPrice : product?.price}`,
                         availability: 'https://schema.org/InStock',
                         priceCurrency: 'USD',
                         itemCondition: 'https://schema.org/NewCondition',
@@ -71,7 +80,151 @@ const CatchAll = ({
                 ]}
             />
             <PageLayoutWrapper>
-
+                <Breadcrumbs
+                    categoryName={body?.body?.category?.category_name}
+                    categorySlug={body?.body?.category?.category_slug}
+                    productName={body?.body?.product_name}
+                />
+                <section className="pt-10 flex flex-col lg:flex-row justify-center relative lg:divide-x-2 lg:divide-black">
+                    <section className="md:pr-16 lg:pr-20">
+                        {isDesktop &&
+                            body?.body?.product_image?.slice(startIdx, endIdx)?.map((image, index) => {
+                                return (
+                                    <div className="relative w-96 h-96">
+                                        <Image
+                                            key={body?.body?.product_name + index}
+                                            src={image.image}
+                                            fill
+                                            objectFit="contain"
+                                            alt={`${body?.body?.product_name} product image ${index + 1}`}
+                                            className="w-auto"
+                                            priority
+                                        />
+                                    </div>
+                                )
+                            })}
+                        {isDesktop && imageLength > 1 &&
+                            <div className='pt-5 flex gap-10 justify-center'>
+                                <button onClick={prevSlide} className="z-10 bg-gray-700 text-white p-2 rounded-full">
+                                    <FaArrowLeft size={30} />
+                                </button>
+                                <button onClick={nextSlide} className="z-10 bg-gray-700 text-white p-2 rounded-full">
+                                    <FaArrowRight size={30} />
+                                </button>
+                            </div>
+                        }
+                    </section>
+                    <section className=" lg:pl-20">
+                        <Link
+                            href={'/' + body?.body?.category?.category_slug}
+                            title={`Shop all ${body?.body?.category?.category_name} products`}
+                            className="underline underline-offset-2 decoration-2"
+                        >Shop all {body?.body?.category?.category_name}</Link>
+                        <h1>{body?.body?.product_name}</h1>
+                        {(isTablet || isMobile) &&
+                            body?.body?.product_image?.slice(startIdx, endIdx)?.map((image, index) => {
+                                return (
+                                    <div className="relative w-64 h-64 md:w-80 md:h-80 mx-auto my-5">
+                                        <Image
+                                            key={body?.body?.product_name + index}
+                                            src={image.image}
+                                            fill
+                                            objectFit="contain"
+                                            alt={`${body?.body?.product_name} product image ${index + 1}`}
+                                            className="w-auto"
+                                            priority
+                                        />
+                                    </div>
+                                )
+                            })
+                        }
+                        {(isTablet || isMobile) && imageLength > 1 &&
+                            <div className='pt-5 flex gap-10 justify-center'>
+                                <button onClick={prevSlide} className="z-10 bg-gray-700 text-white p-2 rounded-full">
+                                    <FaArrowLeft size={30} />
+                                </button>
+                                <button onClick={nextSlide} className="z-10 bg-gray-700 text-white p-2 rounded-full">
+                                    <FaArrowRight size={30} />
+                                </button>
+                            </div>
+                        }
+                        <div className={cx("relative flex", {
+                            ['flex-col']: (isMobile || isDesktop),
+                            ['flex-row gap-5 pr10']: (isTablet),
+                        })}>
+                            <div>
+                                <div
+                                    className={cx("flex", {
+                                        ['items-baseline']: product.discount > 0,
+                                    })}
+                                >
+                                    {product.discount > 0 &&
+                                        <>
+                                            <div className='text-2xl sm:text-3xl text-altRed font-bold'>${product.discountPrice.toFixed(2)}</div>
+                                            <div>
+                                                reg. <span className='line-through'>${product?.price.toFixed(2)}</span>
+                                            </div>
+                                        </>
+                                    }
+                                    {product.discount === 0 &&
+                                        <div className='text-2xl sm:text-3xl text-black font-bold'>${product?.price.toFixed(2)}</div>
+                                    }
+                                </div>
+                                {product?.discount > 0 &&
+                                    <div className="text-md py-1">
+                                        <span className="text-altRed font-bold pr-1.5">Sale</span>
+                                        save ${(product?.price - product?.discountPrice).toFixed(2)} ({(product?.discount * 100).toFixed(0)}% off)
+                                    </div>
+                                }
+                                <div className="text-lg">Estimated shipping cost: ${product?.shippingPrice.toFixed(2)}</div>
+                                <div className="font-bold text-lg">Ship to 00000</div>
+                                {body?.body?.warning &&
+                                    <div className="flex items-center py-3">
+                                        <div className="text-altRed pr-1">
+                                            <GoAlertFill size={20} />
+                                        </div>
+                                        <div>
+                                            <span className="font-bold">Warning: </span>
+                                            {body?.body?.warning}
+                                        </div>
+                                    </div>
+                                }
+                            </div>
+                            <div className="">
+                                <ATCForm productSku={body?.body?.product_sku} />
+                            </div>
+                        </div>
+                    </section>
+                </section>
+                <section className="md:py-10 lg:py-20">
+                    <h2 className="font-optiscript text-center">{body?.body?.headline}</h2>
+                    <h2 className="sr-only">{body?.body?.headline}</h2>
+                    <Accordion
+                        title="Description"
+                        content={body?.body?.description}
+                    />
+                    {body?.body?.additional_description &&
+                        <Accordion
+                            title="Additional Information"
+                            content={body?.body?.additional_description}
+                        />
+                    }
+                    {body?.body?.warning &&
+                        <div className="flex items-center justify-center py-3 text-2xl">
+                            <div className="text-altRed pr-1">
+                                <GoAlertFill size={30} />
+                            </div>
+                            <div>
+                                <span className="font-bold">Warning: </span>
+                                {body?.body?.warning}
+                            </div>
+                        </div>
+                    }
+                    <ProductCarousel
+                        carouselTitle={body?.body?.product_carousel_title}
+                        dataEndpoint={`${slugifiedCategoryName}-products`}
+                    />
+                </section>
             </PageLayoutWrapper>
         </>
     )
