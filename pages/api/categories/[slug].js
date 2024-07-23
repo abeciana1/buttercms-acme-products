c07b4bb44df2ad761c69389d84a46c1063272f5f
abeciana1 <alex.beciana@gmail.com>
2024-07-22 22:03:52 -0600

    Added condition for all categories and named

--- pages/api/categories/[slug].js
+++ pages/api/categories/[slug].js
@@ -3,28 +3,34 @@ import prisma from '@/lib/prisma'
 const handler = async (req, res) => {
     if (req.method === 'GET') {
         let category = req.query.slug
-        const products = await prisma.category.findFirst({
-            where: {
-                slug: {
-                    equals: category,
-                    mode: 'insensitive',
-                }
-            },
-            include: {
-                products: {
-                    select: {
-                        name: true,
-                        discount: true,
-                        price: true,
-                        mainImage: true,
-                        discountPrice: true,
-                        sku: true
+        console.log('!!category', category)
+        if (category === 'all') {
+            const products = await prisma.product.findMany()
+            res.status(200).json(products)
+        } else {
+            const products = await prisma.category.findFirst({
+                where: {
+                    slug: {
+                        equals: category,
+                        mode: 'insensitive',
+                    }
+                },
+                include: {
+                    products: {
+                        select: {
+                            name: true,
+                            discount: true,
+                            price: true,
+                            mainImage: true,
+                            discountPrice: true,
+                            sku: true
+                        }
                     }
                 }
-            }
-        })
-        console.log('products', products)
-        res.status(200).json(products)
+            })
+            console.log('products', products)
+            res.status(200).json(products?.products)
+        }
     }
 }
 
