b2228fc0accb5c6970fcad9c7438d04976e5a0da
abeciana1 <alex.beciana@gmail.com>
2024-07-23 11:31:20 -0600

    Created API routes for demo

---
+++ pages/api/gadget-products.js
@@ -0,0 +1,28 @@
+import prisma from '@/lib/prisma'
+
+const handler = async (req, res) => {
+    if (req.method === 'GET') {
+        const products = await prisma.category.findFirst({
+            where: {
+                slug: {
+                    equals: 'gadget'
+                }
+            },
+            include: {
+                products: {
+                    select: {
+                        name: true,
+                        discount: true,
+                        price: true,
+                        mainImage: true,
+                        discountPrice: true,
+                        sku: true
+                    }
+                }
+            }
+        })
+        res.status(200).json(products?.products)
+    }
+}
+
+export default handler
