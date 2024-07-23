7f7a158b08f74d386b023cfee143fca25c8d143e
abeciana1 <alex.beciana@gmail.com>
2024-07-22 22:05:43 -0600

    Added bottom padding

--- components/_page-sections/ProductGrid/index.js
+++ components/_page-sections/ProductGrid/index.js
@@ -4,7 +4,7 @@ const ProductGrid = ({
     products
 }) => {
     return (
-        <section className="flex flex-wrap gap-5 justify-center">
+        <section className="flex flex-wrap gap-5 justify-center pb-20">
             {products.map(product => (
                 <ProductCard key={product.sku} product={product} />
             ))}
