4ab8593690466474054f732b083a6622672cd400
abeciana1 <alex.beciana@gmail.com>
2024-07-25 11:24:17 -0600

    Removed error handling with div and console log

--- components/ComponentRenderer/index.js
+++ components/ComponentRenderer/index.js
@@ -2,21 +2,18 @@ import dynamic from "next/dynamic"
 import Preloader from "../Preloader"
 import { toPascalCase } from "@/lib/helper-functions"
 import camelcaseKeys from 'camelcase-keys';
-
+import MissingSection from '@/components/_page-sections/MissingSection'
 
 export default function ComponentRenderer({ type, sectionData, products = [] }) {
 	const sectionsComponentPaths = () => ({
 		[type]: dynamic(
 			() => import(`@/components/_page-sections/${toPascalCase(type)}`)
-				.catch((error) => () => {
-				console.log(error)
-				return <div>error</div>
-			}), {
+				.catch(() => () => MissingSection), {
 			loading: Preloader,
             ssr: true
 		})
 	});
-	const SectionComponent = sectionsComponentPaths()[type] || <div>Missing component: {toPascalCase(type)}</div>;
+	const SectionComponent = sectionsComponentPaths()[type] || MissingSection;
 
 	return <SectionComponent type={type} {...camelcaseKeys(sectionData)} products={products} />
 }
