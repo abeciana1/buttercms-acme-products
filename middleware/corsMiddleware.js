153580120e404be55d28573bc82c2662e2769bff
abeciana1 <alex.beciana@gmail.com>
2024-07-25 11:33:32 -0600

    Adjusted middleware error messaging

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -8,14 +8,14 @@ const cors = Cors({
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true)
         } else {
-            callback(new Error('Not allowed by CORS'), false);
+            callback(new Error(`Not allowed by CORS: ${origin}`), false);
         }
     },
     credentials: true,
     allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Content-Type', 'X-Api-Version']
 });
 
-export const runMiddleware = async (req, res) => {
+export const runMiddleware = (req, res) => {
     return new Promise((resolve, reject) => {
         cors(req, res, (result) => {
         if (result instanceof Error) {
