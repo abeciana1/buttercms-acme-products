c8850a429e4a8be4d2c59d5f07df8503fdfccbeb
abeciana1 <alex.beciana@gmail.com>
2024-07-24 20:04:16 -0600

    Created catch all dynamic page

---
+++ pages/[...slug].js
@@ -0,0 +1,54 @@
+import { getPageData } from '@/lib/api'
+import { NextSeo } from 'next-seo'
+import ComponentRenderer from '@/components/ComponentRenderer'
+import { PageLayoutWrapper } from '@/components/_layouts'
+
+const DynamicPage = (props) => {
+    console.log('pops', props)
+    return (
+        <>
+            <NextSeo
+                title={props?.seo?.title}
+                description={props?.seo?.description}
+            />
+            <PageLayoutWrapper>
+                {props?.body?.body?.map(({ type, fields: sectionData}, index) => {
+                    return <ComponentRenderer
+                        key={type + index}
+                        type={type}
+                        sectionData={sectionData}
+                    />
+                })}
+            </PageLayoutWrapper>
+        </>
+    )
+}
+
+export default DynamicPage
+
+export const getServerSideProps = async (context) => {
+    try {
+        let path = context?.query?.slug
+        const page = await getPageData('page', path)
+        return {
+            props: {
+                seo: {
+                    title: page?.body?.seo?.title,
+                    description: page?.body?.seo?.description,
+                },
+                body: page?.body,
+            }
+        }
+    } catch (error) {
+        const page = await getPageData('page', '404')
+        return {
+            props: {
+                seo: {
+                    title: page?.body?.seo?.title,
+                    description: page?.body?.seo?.description,
+                },
+                body: page?.body,
+            }
+        }
+    }
+}
