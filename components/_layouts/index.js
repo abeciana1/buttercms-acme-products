aaf67d517969a713f39f64461849414b53fbe590
abeciana1 <alex.beciana@gmail.com>
2024-07-25 16:12:21 -0600

    Indent added

--- components/_layouts/index.js
+++ components/_layouts/index.js
@@ -1,7 +1,7 @@
 export const PageLayoutWrapper = ({ children }) => {
     return (
         <section className='px-5 md:px-10 py-5 md:py-10'>
-            { children }
+        { children }
         </section>
     )
 }
