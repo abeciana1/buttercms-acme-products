f217b23b4209d0e5c4d741b710b9ddad588494ed
abeciana1 <alex.beciana@gmail.com>
2024-08-07 12:35:30 -0600

    Created component for missing form field case

---
+++ components/_forms/fields/MissingField/index.js
@@ -0,0 +1,9 @@
+export default function MissingField({ type, ...sectionData }) {
+    console.log(`Missing form field ${type} data ${sectionData}`)
+    return (
+    <div>
+        <p>Missing a template for {type} form field</p>
+        <p>Check console for component details</p>
+    </div>
+    )
+}
