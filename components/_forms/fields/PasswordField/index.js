9671cb21613b9496376c91ed96f557ca90808b6e
abeciana1 <alex.beciana@gmail.com>
2024-08-08 16:28:46 -0600

    Created PasswordField component

---
+++ components/_forms/fields/PasswordField/index.js
@@ -0,0 +1,44 @@
+import { ErrorMessage } from "@hookform/error-message"
+import { createPattern, createPatternMessage } from '@/lib/helper-functions'
+
+const PasswordField = ({
+    id,
+    label,
+    name,
+    required,
+    validationError,
+    register,
+    errors,
+    allowNumbers = false,
+    allowSpecialChars = false
+}) => {
+    const pattern = createPattern(allowNumbers, allowSpecialChars);
+    const patternMessage = createPatternMessage(allowNumbers, allowSpecialChars);
+
+    return (
+        <>
+            <label htmlFor={`${name}-${id}`}>{label}{(validationError || required) ? '*' : ''}</label>
+            <input
+                id={`${name}-${id}`}
+                type='password'
+                name={name}
+                aria-label={label}
+                className='border-2 border-altBlack rounded-lg py-0.5 px-1'
+                {...register(name, {
+                    required: required ? { value: true, message: validationError || "This field is required" } : { value: false },
+                    pattern: {
+                        value: pattern,
+                        message: patternMessage
+                    }
+                })}
+            />
+            <ErrorMessage
+                errors={errors}
+                name={name}
+                render={({ message }) => <p className='text-altRed'>{message}</p>}
+            />
+        </>
+    )
+}
+
+export default PasswordField;
