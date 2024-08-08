167dcebe81fecd8a6510bcd6603e36f11247f212
abeciana1 <alex.beciana@gmail.com>
2024-08-08 15:10:50 -0600

    Created TextField component

---
+++ components/_forms/fields/TextField/index.js
@@ -0,0 +1,44 @@
+import { ErrorMessage } from "@hookform/error-message"
+import { useMemo } from 'react'
+
+const TextField = ({
+    id,
+    label,
+    name,
+    required,
+    validationError,
+    minNumber,
+    maxNumber,
+    register,
+    errors
+}) => {
+    // const memoizedErrorMessage = useMemo(() => (
+    //     <ErrorMessage
+    //         errors={errors}
+    //         name={name}
+    //         render={({ message }) => <p className='text-altRed'>{message}</p>}
+    //     />
+    // ), [errors, name]);
+    return (
+        <>
+            <label htmlFor={`${name}-${id}`}>{label}{(validationError || required) ? '*' : ''}</label>
+            <input
+                id={`${name}-${id}`}
+                type='text'
+                name={name}
+                aria-label={label}
+                className='border-2 border-altBlack rounded-lg py-0.5 px-1'
+                {...register(name, {
+                    required: required ? { value: true, message: validationError || "This field is required" } : { value: false }
+                })}
+            />
+            <ErrorMessage
+                errors={errors}
+                name={name}
+                render={({ message }) => <p className='text-altRed'>{message}</p>}
+            />
+        </>
+    )
+}
+
+export default TextField;
