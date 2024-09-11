7b5917ff2f535d62554c6929c81bca415ccdd5ff
abeciana1 <alex.beciana@gmail.com>
2024-09-11 14:41:02 -0600

    Created api endpoint to return all products in array payload

---
+++ pages/api/adhoc-products/index.js
@@ -0,0 +1,30 @@
+import prisma from '@/lib/prisma'
+import { runMiddleware } from '@/middleware/corsMiddleware'
+
+const handler = async (req, res) => {
+    await runMiddleware(req, res)
+    if (req.method === 'POST') {
+        let cleanedProductList = req.body.data?.map(product => product.product_sku)
+        console.log('cleanedProductList', cleanedProductList)
+        const products = await prisma.product.findMany({
+            where: {
+                sku: {
+                    in: cleanedProductList
+                }
+            },
+            select: {
+                sku:true,
+                name: true,
+                price: true,
+                discount: true,
+                discountPrice: true,
+                headline: true,
+                mainImage: true,
+                shippingPrice: true
+            }
+        })
+        res.status(200).json(products)
+    }
+}
+
+export default handler
