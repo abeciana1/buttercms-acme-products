da0d61037b3bd68a7ada0e18f7619bbb20891749
abeciana1 <alex.beciana@gmail.com>
2024-09-17 12:31:54 -0600

    Added button functionality to turn persona cookies on/off

--- pages/personalize.js
+++ pages/personalize.js
@@ -1,7 +1,45 @@
+import { useState } from 'react'
 import { NextSeo } from 'next-seo'
 import { PageLayoutWrapper } from '@/components/_layouts'
+import OnClickButton from '@/components/_inputs/Buttons/OnClickButton'
+import Cookies from 'js-cookie'
+
 
 const Personalize = () => {
+    const [ coyotePersona, setCoyotePersona ] = useState(Cookies.get('coyote-promo') ? true : false)
+    const [ roadrunnerPersona, setRoadrunnerPersona ] = useState(Cookies.get('roadrunner-promo') ? true : false)
+    console.log('Coyote cookies', Cookies.get('coyote-promo'))
+    console.log('Roadrunner cookies', Cookies.get('roadrunner-promo'))
+
+    // Personalization functions
+    const setPersonaCookie = (persona) => {
+        if (persona === 'coyote') {
+            if (Cookies.get('roadrunner-promo')) {
+                Cookies.remove('roadrunner-promo')
+                setRoadrunnerPersona(false)
+            }
+            if (Cookies.get('coyote-promo')) {
+                Cookies.remove('coyote-promo')
+                setCoyotePersona(false)
+            } else {
+                Cookies.set('coyote-promo', true, { expires: 1 })
+                setCoyotePersona(true)
+            }
+        } else {
+            if (Cookies.get('coyote-promo')) {
+                Cookies.remove('coyote-promo')
+                setCoyotePersona(false)
+            }
+            if (Cookies.get('roadrunner-promo')) {
+                Cookies.remove('roadrunner-promo')
+                setRoadrunnerPersona(false)
+            } else {
+                Cookies.set('roadrunner-promo', true, { expires: 1 })
+                setRoadrunnerPersona(true)
+            }
+        }
+    }
+
     return (
         <>
             <NextSeo
@@ -12,9 +50,20 @@ const Personalize = () => {
             />
             <PageLayoutWrapper>
                 <h1>Personalization content powered by Butter</h1>
-                <section>
+                <section className='py-10'>
                     <h2>Personalization on a user level</h2>
-                    
+                    <div className='space-y-5 max-w-fit'>
+                        <OnClickButton
+                            buttonText={`${coyotePersona ? 'Disable' : 'Enable'} Coyote persona`}
+                            color={coyotePersona ? 'Red': 'Green'}
+                            onClick={() => setPersonaCookie('coyote')}
+                        />
+                        <OnClickButton
+                            buttonText={`${roadrunnerPersona ? 'Disable' : 'Enable'} Roadrunner persona`}
+                            color={roadrunnerPersona ? 'Red': 'Green'}
+                            onClick={() => setPersonaCookie('roadrunner')}
+                        />
+                    </div>
                 </section>
                 <section>
                     <h2>Promotional popups</h2>
