cf6d65585a82e50080707ee3649ed2a866a5f273
abeciana1 <alex.beciana@gmail.com>
2024-09-19 16:43:44 -0600

    Added x-axis padding

--- components/_inputs/Buttons/OnClickButton/index.js
+++ components/_inputs/Buttons/OnClickButton/index.js
@@ -8,7 +8,7 @@ const OnClickButton = ({
     return (
         <button
             onClick={onClick}
-            className={cx('w-full h-10 min-w-32 sm:min-w-44 text-lg rounded-md py-1', {
+            className={cx('w-full h-10 min-w-32 sm:min-w-44 text-lg rounded-md py-1 px-2', {
                 ['text-white bg-altRed ']: color === 'Red',
                 ['text-white bg-utilityGreen ']: color === 'Green',
             })}
