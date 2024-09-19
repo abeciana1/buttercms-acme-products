428264a11839a68301c63b20553f5f560cb9f4ad
abeciana1 <alex.beciana@gmail.com>
2024-09-19 15:28:54 -0600

    Imported and added instance to combined reducers

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '@/redux/slices/cartSlice'
+import instanceReducer from '@/redux/slices/instanceSlice'
 
 const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        instance: instanceReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
