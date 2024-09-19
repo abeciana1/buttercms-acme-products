b74c1e5bba41b1099634525c51323c55846e60c0
abeciana1 <alex.beciana@gmail.com>
2024-09-19 15:29:16 -0600

    Removed console log

--- components/_page-sections/Jotform/index.js
+++ components/_page-sections/Jotform/index.js
@@ -1,7 +1,6 @@
 import JotformEmbed from 'react-jotform-embed';
 
 const Jotform = ({ form }) => {
-    console.log('props', form)
     return(
         <div>
             <JotformEmbed src={form?.url} />
