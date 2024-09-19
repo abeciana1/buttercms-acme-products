8e7d2df2ae6726a75bab4ef12a8cf0eb4a28f8d0
abeciana1 <alex.beciana@gmail.com>
2024-09-19 16:40:42 -0600

    Added extra conditional logic for form rendering, Added close handler, Added conditional rendering for CTA links

--- components/_modals/PromoPopup/index.js
+++ components/_modals/PromoPopup/index.js
@@ -2,14 +2,23 @@ import { useEffect, useState } from 'react'
 import parse from 'html-react-parser';
 import { getFormCollection } from '@/lib/api'
 import Form from '@/components/_page-sections/Form'
+import { LinkLookLikeButton } from '@/components/_inputs/Links'
+import { useDispatch } from 'react-redux';
+import { clearInstance } from '@/redux/slices/instanceSlice';
 
 const PromoPopup = ({ data }) => {
-    const { text_content, form } = data;
+    const dispatch = useDispatch()
+    const {
+        text_content,
+        form,
+        promo_link_href,
+        promo_button_text,
+        promo_button_color
+    } = data;
     const [ popupForm, setPopupForm ] = useState(null)
-
     
     useEffect(() => {
-        if (form) {
+        if (Object.hasOwn(form, 'form_name')) {
             const collectFormFields = async () => {
                 const formContent = await getFormCollection(form.form_name);
                 setPopupForm(formContent)
@@ -18,12 +27,24 @@ const PromoPopup = ({ data }) => {
         }
     }, [form])
 
+    const closeInstance = () => {
+        dispatch(clearInstance())
+    }
 
     return (
         <section className='max-w-md space-y-5'>
             {parse(text_content)}
             {popupForm &&
                 <Form key={form?.name} form={popupForm} onModal />
+            }
+            {(promo_link_href && promo_button_color && promo_button_text) &&
+                <div className='flex justify-center' onClick={closeInstance}>
+                    <LinkLookLikeButton
+                        linkText={promo_button_text}
+                        linkHref={promo_link_href}
+                        color={promo_button_color.brand_color}
+                    />
+                </div>
             }
         </section>
     )
